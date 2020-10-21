02981a2e0dce335905b71054be23d5b4b8e24031
Christoffer A Traeen <christoffer.andersen@live.com>
2020-10-21 11:14:19 +0200

    Design assets

--- lib/designstuff/themes.dart
+++ lib/designstuff/themes.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+var formTextColor = Color(0xFF555555);
+var formHintColor = Color(0xFFCCCCCC);
+var errorColor = Color(0xFFD96060);
+var acceptColor = Color(0xFF4F6E81);
+var softTextColor = Color(0xFFD5E2E6);
+var fadeTextColor = Color(0xFF687A89);
+var mainBackgroundColor = Color(0xFF161D21);
+var elementBackgroundColor = Color(0xFF222D33);
+var linkActiveColor = Color(0xFFFFC891);
+var splitLineColor = Color(0xFF304048);
+
 var alpha = Color(0xffff0099);
 var beta = Color(0xff00cc0c);
 var charly = Color(0xff000099);

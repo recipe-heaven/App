bb45a4471249d4176e8dd09c2ab7ba20c9395aeb
trygve <trygve.woldseth@gmail.com>
2020-11-12 13:50:58 +0100

    generated

---
+++ lib/data_classes/food_image.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'food_image.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FoodImage _$FoodImageFromJson(Map<String, dynamic> json) {
+  return FoodImage()
+    ..id = json['id'] as int
+    ..name = json['name'] as String
+    ..mimeType = json['mimeType'] as String
+    ..size = json['size'] as int;
+}
+
+Map<String, dynamic> _$FoodImageToJson(FoodImage instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  writeNotNull('name', instance.name);
+  writeNotNull('mimeType', instance.mimeType);
+  writeNotNull('size', instance.size);
+  return val;
+}

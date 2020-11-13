643d27fe5b423a434b58cf10959fdcc61ae8e4d0
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-13 22:26:32 +0100

    Add enum helper

---
+++ lib/helpers/enumHelper.dart
@@ -0,0 +1,12 @@
+import 'package:App/data_classes/recipe.dart';
+
+/// Returns the value of the enum type as string
+String getStringFromEnum(enumType) {
+  return enumType.toString().split('.')[1];
+}
+
+/// Returns the enum type for the provided enum string representation
+getEnumType(enumType, stringRepresentation) {
+  return enumType.values
+      .firstWhere((e) => getStringFromEnum(e) == stringRepresentation);
+}

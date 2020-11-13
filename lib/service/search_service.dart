f4601dffb88cf1e53daa2450e157b7487f6cf66f
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-13 19:42:26 +0100

    Authorize search

--- lib/service/search_service.dart
+++ lib/service/search_service.dart
@@ -4,6 +4,7 @@ import 'package:App/pages/explore/result.dart';
 import 'package:App/pages/explore/search_options.dart';
 import 'package:App/service/endpoints.dart';
 import 'package:App/service/http_client.dart';
+import 'package:App/store/store.dart';
 
 class SearchService {
   final Http httpClient;
@@ -12,8 +13,9 @@ class SearchService {
 
   Future<Result> search(SearchOptions options) async {
     try {
+      final token = await Storage().getToken();
       var response = await httpClient.post(searchEndpoint,
-          headers: {'Content-type': "application/json"},
+          headers: {'Content-type': "application/json", "Authorization": token},
           body: options.toJsonString());
       if (response.statusCode == 200) {
         var body = jsonDecode(response.body);

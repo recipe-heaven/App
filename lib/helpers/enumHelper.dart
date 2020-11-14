98b562ffe2ec768de7d786c336c1b483bfb76b72
Christoffer A Træen <christoffer.andersen@live.com>
2020-11-14 12:44:10 +0100

    Update lib/helpers/enumHelper.dart
    Co-authored-by: TrygveJW <48008160+TrygveJW@users.noreply.github.com>

--- lib/helpers/enumHelper.dart
+++ lib/helpers/enumHelper.dart
@@ -2,7 +2,7 @@ import 'package:App/data_classes/recipe.dart';
 
 /// Returns the value of the enum type as string
 String getStringFromEnum(enumType) {
-  return enumType.toString().split('.')[1];
+  return describeEnum(enumtype);
 }
 
 /// Returns the enum type for the provided enum string representation

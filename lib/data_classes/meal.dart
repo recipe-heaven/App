5c723a3a54c816793c5aef8279c917d50c85c238
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-17 16:59:40 +0100

    Create meal

--- lib/data_classes/meal.dart
+++ lib/data_classes/meal.dart
@@ -1,7 +1,7 @@
+import 'dart:convert';
 import 'dart:ui';
 
 import 'package:App/data_classes/recipe.dart';
-import 'package:App/data_classes/user.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:flutter/material.dart';
 
@@ -31,4 +31,36 @@ class Meal with UserOwned {
 
   factory Meal.fromJson(Map<String, dynamic> json) => _$MealFromJson(json);
   Map<String, dynamic> toJson() => _$MealToJson(this);
+}
+
+/// A represents a new meal object, this can be serialized
+/// and sent to the server for storing the new meal
+@JsonSerializable(explicitToJson: true)
+class NewMeal {
+  String _name;
+
+  bool _public = false;
+
+  List<Map<String, int>> _recipes = new List();
+
+  NewMeal(String name, bool public, List<int> recipes) {
+    this._name = name;
+    this._public = public;
+    this._recipes = this.createRecipesIdObjects(recipes);
+  }
+
+  List<Map<String, int>> createRecipesIdObjects(List<int> recipeIds) {
+    List<Map<String, int>> recipemap = new List();
+    for (var id in recipeIds) {
+      recipemap.add({"id": id});
+    }
+    return recipemap;
+  }
+
+  String get name => _name;
+  bool get public => _public;
+  List<Map<String, int>> get recipes => _recipes;
+
+  String toJsonString() => jsonEncode(_$NewMealToJson(this));
+  Map<String, dynamic> toJson() => _$NewMealToJson(this);
 }
--- lib/service/meal_service.dart
+++ lib/service/meal_service.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:App/data_classes/meal.dart';
 import 'package:App/service/endpoints.dart';
 import 'package:App/service/http_client.dart';
+import 'package:App/store/store.dart';
 import 'package:flutter/foundation.dart';
 
 class MealService {
@@ -8,11 +12,19 @@ class MealService {
 
   MealService(this.httpClient);
 
-  Future<bool> addNewMeal({@required Meal meal}) async {
-    var response = await httpClient.post(newMealEndpoint, body: meal.toJson());
-    if (response.statusCode == 200) {
-      return true;
-    } else {
+  Future<bool> addNewMeal({@required NewMeal newMeal}) async {
+    try {
+      final token = await Storage().getToken();
+      var response = await httpClient.post(newMealEndpoint,
+          headers: {'Content-type': "application/json", "Authorization": token},
+          body: newMeal.toJsonString());
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } on IOException {
       return false;
     }
   }

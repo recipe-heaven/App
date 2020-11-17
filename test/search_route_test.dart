3b4613def9d6f670a0712786ebaef1dee22f251f
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-17 17:05:29 +0100

    Search route test

--- test/search_route_test.dart
+++ test/search_route_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('search route has only selector', () {
-    var p = SearchRouteOptions(onlyMenus: true);
-    expect(p.hasOnlySelector(), true);
+    var p = SearchRouteOptions(searchMenus: false, searchRecipes: false);
+    expect(p.hasOnlyOneSearchSelector(), true);
   });
 }

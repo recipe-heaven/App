5de582004ea82c31b894bb2297d5a99a1e033abd
Christoffer A Traeen <christoffer.andersen@live.com>
2020-11-30 23:20:33 +0100

    Update change password page

--- lib/pages/profile/change_password.dart
+++ lib/pages/profile/change_password.dart
@@ -1,28 +1,24 @@
+import 'package:App/app_state.dart';
 import 'package:App/components/form/form_validators.dart' as validators;
 import 'package:App/components/navigation_scaffold.dart';
-import 'package:App/data_classes/user.dart';
 import 'package:App/components/input_feald.dart';
+import 'package:App/components/round_button.dart';
+import 'package:App/components/saved_snackbar.dart';
 import 'package:App/service/http_client.dart';
 import 'package:App/service/user_service.dart';
-import 'package:App/theme/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
 
 class _ChangePassForm {
-  String oldPass;
-  String newPass;
-  String newPassVerified;
+  String oldPass = "";
+  String newPass = "";
+  String newPassVerified = "";
 }
 
 class ChangePassPage extends StatefulWidget {
   final _userService = UserService(HttpServiceClient());
   final _formData = _ChangePassForm();
-  final _user = User(
-      id: 11,
-      email: "skdaj",
-      name: "navn test",
-      username: "usernav",
-      settings: null);
 
   ChangePassPage({Key key}) : super(key: key);
 
@@ -33,103 +29,100 @@ class ChangePassPage extends StatefulWidget {
 class ChangePassPageState extends State<ChangePassPage> {
   final _formKey = GlobalKey<FormState>();
 
-  void _handleChangePass() async {
+  void _handleChangePass(BuildContext context) async {
     final FormState formState = _formKey.currentState;
+    formState.save();
     if (formState.validate()) {
-      formState.save();
-
-      var res = await widget._userService.changePass(
+      var success = await widget._userService.changePass(
           newPass: widget._formData.newPass, oldPass: widget._formData.oldPass);
-      if (res) {
-        print("\n\nsucsess\n\n");
+      if (success) {
+        displaySavedSnackbar("password", context, updated: true);
       } else {
-        print("\n\nerror\n\n");
+        displaySavedSnackbar("password", context, error: true);
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    var user = Provider.of<AppState>(context).user;
     return ScaffoldWithNavigation(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 50),
-              child: Text(
-                widget._user.name,
-                style: Theme.of(context).textTheme.headline1,
-              ),
-            ),
-            Column(
+      body: Builder(
+        builder: (context) => SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  height: 40,
-                ),
-                SizedBox(
-                  height: 70,
+                Text(
+                  "Hi",
+                  style: Theme.of(context).primaryTextTheme.headline5,
                 ),
                 Text(
-                  "Change Password",
+                  user.name,
                   style: Theme.of(context).textTheme.headline1,
                 ),
-                SizedBox(
-                  height: 10,
+                Column(
+                  children: [
+                    Divider(
+                      color: Theme.of(context).dividerColor,
+                      thickness: .5,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      "Change Password",
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Form(
+                        key: _formKey,
+                        child: Column(children: [
+                          newMealInputBox(context,
+                              label: "New password",
+                              onSave: (newValue) =>
+                                  widget._formData.newPass = newValue,
+                              validator: (value) {
+                                return validators.multivalidate([
+                                  () => validators.validateNotEmptyInput(value),
+                                  () => validators.validateLength(value, min: 6)
+                                ]);
+                              },
+                              obscureInput: true,
+                              hint: "New password"),
+                          newMealInputBox(context,
+                              label: "Retype new password",
+                              onSave: (newValue) =>
+                                  widget._formData.newPassVerified = newValue,
+                              validator: (value) {
+                                return validators.multivalidate([
+                                  () => validators.validateNotEmptyInput(value),
+                                  () => validators.validateEquality(value,
+                                      widget._formData.newPass, "new password")
+                                ]);
+                              },
+                              obscureInput: true,
+                              hint: "Retype new password"),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20, bottom: 20),
+                            child: RoundButton(
+                              () {
+                                _handleChangePass(context);
+                              },
+                              buttonText: "Save",
+                            ),
+                          )
+                        ])),
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                 ),
-                Form(
-                    key: _formKey,
-                    child: Column(children: [
-                      newMealInputBox(context,
-                          label: "Old Password",
-                          onSave: (newValue) =>
-                              {widget._formData.oldPass = newValue},
-                          validator: validators.validateNotEmptyInput,
-                          hint: "Old Password"),
-                      newMealInputBox(context,
-                          label: "New password",
-                          onSave: (newValue) => {widget._formData.newPass},
-                          validator: validators.validateNotEmptyInput,
-                          obscureInput: true,
-                          hint: "New password"),
-                      newMealInputBox(context,
-                          label: "Retype new password",
-                          onSave: (newValue) => {print("ccccc")},
-                          validator: (value) {
-                            if (value != widget._formData.newPass) {
-                              return "passwords do not match";
-                            }
-                            return null;
-                          },
-                          obscureInput: true,
-                          hint: "Retype new password"),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 20),
-                        child: MaterialButton(
-                          onPressed: _handleChangePass,
-                          child: Text(
-                            "Change password",
-                            style: Theme.of(context).textTheme.headline2,
-                          ),
-                          minWidth: double.infinity,
-                          color: acceptColor,
-                        ),
-                      ),
-                      MaterialButton(
-                        onPressed: _handleChangePass,
-                        child: Text(
-                          "Delete user",
-                          style: Theme.of(context).textTheme.headline2,
-                        ),
-                        minWidth: double.infinity,
-                        color: errorColor,
-                      )
-                    ])),
               ],
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
             ),
-          ],
+          ),
         ),
       ),
     );

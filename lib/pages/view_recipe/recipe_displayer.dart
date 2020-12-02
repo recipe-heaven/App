f8b0f9f3a0fe53ef4555e3afb9b28f96e223b36d
Christoffer A Traeen <christoffer.andersen@live.com>
2020-12-02 11:28:28 +0100

    Adjust recipe and display clock

--- lib/pages/view_recipe/recipe_displayer.dart
+++ lib/pages/view_recipe/recipe_displayer.dart
@@ -1,3 +1,4 @@
+import 'package:App/components/time_widget.dart';
 import 'package:App/data_classes/recipe.dart';
 import 'package:App/data_classes/user.dart';
 import 'package:App/theme/themes.dart';
@@ -33,18 +34,22 @@ class DisplayRecipe extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ShaderMask(
-            shaderCallback: (rect) {
-              return LinearGradient(
-                  begin: Alignment(0.0, 0.0),
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Theme.of(context).scaffoldBackgroundColor,
-                    Colors.transparent
-                  ]).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-            },
-            blendMode: BlendMode.dstIn,
-            child: _recipe.displayImage),
+        Container(
+          height: 300,
+          child: ShaderMask(
+              shaderCallback: (rect) {
+                return LinearGradient(
+                        begin: Alignment(0.0, 0.0),
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      Theme.of(context).scaffoldBackgroundColor,
+                      Colors.transparent
+                    ])
+                    .createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+              },
+              blendMode: BlendMode.dstIn,
+              child: _recipe.displayImage),
+        ),
         Container(
           padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
           child: Column(
@@ -61,39 +66,41 @@ class DisplayRecipe extends StatelessWidget {
                   ],
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 ),
-                Row(
-                  children: [
-                    Icon(Icons.access_time,
-                        color: Theme.of(context).iconTheme.color),
-                    Text(
-                      _recipe.cookTime.toString(),
-                      style: Theme.of(context)
-                          .primaryTextTheme
-                          .headline3
-                          .copyWith(fontSize: 15),
-                    )
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Row(
+                    children: [TimeWidget(timeInSeconds: _recipe.cookTime)],
+                  ),
                 ),
-                Row(
-                  children: [
-                    for (var tag in _recipe.tags) ...[
-                      Text(
-                        tag.tagName,
-                        style: Theme.of(context).primaryTextTheme.headline4,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      )
-                    ]
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Row(
+                    children: [
+                      for (var tag in _recipe.tags) ...[
+                        Text(
+                          tag.tagName,
+                          style: Theme.of(context).primaryTextTheme.headline4,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        )
+                      ]
+                    ],
+                  ),
                 ),
-                Text(
-                  _recipe.description,
-                  style: Theme.of(context).accentTextTheme.bodyText2,
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    _recipe.description,
+                    style: Theme.of(context).accentTextTheme.bodyText2,
+                  ),
                 ),
-                Divider(
-                  color: Theme.of(context).dividerColor,
-                  thickness: .5,
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Divider(
+                    color: Theme.of(context).dividerColor,
+                    thickness: .5,
+                  ),
                 ),
                 Text("Ingredients",
                     style: Theme.of(context).textTheme.headline2),
@@ -110,58 +117,61 @@ class DisplayRecipe extends StatelessWidget {
                 ),
               ]),
         ),
-        Column(
-          children: [
-            for (MapEntry<int, RecipeStep> entry
-                in _recipe.cookingSteps.asMap().entries) ...[
-              Container(
-                child: Stack(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 15, horizontal: 0),
-                      child: Container(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 10),
-                          child: Text(
-                            entry.value.step,
-                            style: Theme.of(context).accentTextTheme.bodyText2,
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Column(
+            children: [
+              for (MapEntry<int, RecipeStep> entry
+                  in _recipe.cookingSteps.asMap().entries) ...[
+                Container(
+                  child: Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 0),
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 10),
+                            child: Text(
+                              entry.value.step,
+                              style:
+                                  Theme.of(context).accentTextTheme.bodyText2,
+                            ),
                           ),
+                          color: recipeStepDescriptionBackground,
+                          width: double.infinity,
                         ),
-                        color: recipeStepDescriptionBackground,
-                        width: double.infinity,
                       ),
-                    ),
-                    Container(
-                      child: Stack(
-                        children: [
-                          Image.asset(
-                            "assets/images/step-flag.png",
-                            width: 56,
-                            height: 36,
-                          ),
-                          Text(
-                            (entry.key + 1).toString(),
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.left,
-                          ),
-                        ],
-                        alignment: Alignment(-0.6, -.4),
+                      Container(
+                        child: Stack(
+                          children: [
+                            Image.asset(
+                              "assets/images/step-flag.png",
+                              width: 56,
+                              height: 36,
+                            ),
+                            Text(
+                              (entry.key + 1).toString(),
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                          alignment: Alignment(-0.6, -.4),
+                        ),
+                        alignment: Alignment(-1, 0),
                       ),
-                      alignment: Alignment(-1, 0),
-                    ),
-                  ],
+                    ],
+                  ),
+                  padding: const EdgeInsets.only(top: 10),
                 ),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
-              ),
-            ]
-          ],
-          crossAxisAlignment: CrossAxisAlignment.start,
+              ]
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
         )
       ],
     );
